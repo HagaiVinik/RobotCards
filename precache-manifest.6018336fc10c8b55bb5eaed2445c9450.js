47c41b982b0a1029b452676af434f2fcd6c01475
Hagai <hagai.v9@gmail.com>
2020-04-02 15:08:41 +0300

    Updates

---
+++ precache-manifest.6018336fc10c8b55bb5eaed2445c9450.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3928e27a0a8d7507607014764750d3c",
+    "url": "/RobotCards/index.html"
+  },
+  {
+    "revision": "5cd12b4ab43d0295ca7d",
+    "url": "/RobotCards/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "597acf5d51b269467587",
+    "url": "/RobotCards/static/css/main.873836e1.chunk.css"
+  },
+  {
+    "revision": "5cd12b4ab43d0295ca7d",
+    "url": "/RobotCards/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "597acf5d51b269467587",
+    "url": "/RobotCards/static/js/main.ec66c5f1.chunk.js"
+  },
+  {
+    "revision": "57064a72ee9e30e2045e",
+    "url": "/RobotCards/static/js/runtime~main.08aad59e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RobotCards/static/media/SEGA.12a3db8b.woff"
+  }
+]);
